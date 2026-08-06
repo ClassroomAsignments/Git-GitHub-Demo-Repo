443a31ce7b848616aa43307bb2e71fb1af5cc397
FA-TeamLead <farooque.azam@gmail.com>
2026-08-06 10:12:18 +0000

    style: update app greeting on main branch

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ Application Main Module - Task 4 Starter File
 """
 
 def greet(name):
-    return f"Greetings from main branch, {name}!"
+    return f"Hello from main branch, {name}!"
 
 if __name__ == "__main__":
     print(greet("Student"))
