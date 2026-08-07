9855580e2ebfa7c9fefe7c238b23adc8aca9094c
farooque-azam <farooq@ceme.nust.edu.pk>
2026-08-07 14:28:47 +0500

    Initial assignment setup

---
+++ src/calculator.py
@@ -0,0 +1,12 @@
+"""
+Calculator Module - Task 3 Starter File
+Implement the functions below on the 'feature/calculator' branch.
+"""
+
+def add(a, b):
+    # TODO: Implement addition
+    pass
+
+def multiply(a, b):
+    # TODO: Implement multiplication
+    pass
