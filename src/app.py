c5e634adb287d19a299689ea90f2eca706f47832
FA-TeamLead <farooque.azam@gmail.com>
2026-08-07 07:37:43 +0000

    style: update app greeting on conflict branch

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ Application Main Module - Task 4 Starter File
 """
 
 def greet(name):
-    return f"Greetings from main branch, {name}!"
+    return "Hello from conflict branch!"
 
 if __name__ == "__main__":
     print(greet("Student"))
